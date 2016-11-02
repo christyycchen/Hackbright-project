2b598ce0513b7d313c4279137207340285237edf
Christy Chen <christychen126@gmail.com>
2016-11-02 04:11:15 +0000

    Add outline to server.py, create templets

--- server.py
+++ server.py
@@ -20,24 +20,109 @@ app.secret_key = "ABC"
 
 # undefined variable in Jinja2 raises an error
 app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+
+
+@app.route('/')
+def index():
+    """landing page"""
+
+    if session.get("user_id"):
+        return redirect('/home')
+    else: 
+        return render_template('index.html')
+
+
+@app.route('login', methods=["POST"])
+def login():
+
+    input_username = request.form.get() #username 
+    input_password = request.form.get() #password
+
+
+    #if email in db, 
+        #if password match
+            #save user in session 
+            return redirect('/home')
+        #if password doesnt match 
+            #flash password doesnt match message
+            return redirect('/')
+    #else:
+        #flash username not found, please register
+        return redirect('/')
+
+
+
+@app.route('register', methods=["POST"])
+def register():
+
+    input_username = request.form.get() #username 
+    input_password = request.form.get() #password
+    input_user_airport = request.form.get() #airport ---how to reinforce this?
+
+    #if username does not exist
+        #write user into db
+        #add user to session
+        return redirect('/home')
+    #else 
+        #flash you already registered before, do you want to log in?
+        return redirect('/')
 
 
 
 
+@app.route('/home')
+def homepage():
+    """user homepage"""
+
+    #after logeed in, get user data from database by user_id in session
+    #display user saved trip
+
+    return render_template('homepage.html')
 
 
 
+@app.route('/search_result', methods=["POST"])
+def search_result():
+    """send requests to APIs and display results"""
+
+
+    departure_airport =  #get user default_airport in db
+    destination_airport = #pick an airport 
+
+    input_departure_date = request.form.get()
+    input_return_date =  request.form.get()
+
+    #call the functions to send results to API and get results 
+    
+    #set variables
+
+    return render_template('result_page.html', #pass variables in)
 
 
 
+@app.route('/home/<int:trip_id>')
+def saved_trip(trip_id):
+    """dusplay saved trip with details"""
+
+    #get trip info from db
+
+    return render_template('saved_trip.html')
 
 
 
+@app.route('/home/<int:trip_id>/delete', methods=["POST"])
+def delete_trip(trip_id):
+    """delete a saved trip"""
+
+    #delete trop by id from db
+
+    return redirect('/home')
 
 
 
-
-
+    
 
 
 
